04b2f073b78a52867f6cac16be9fa4558fa46727
Esat Arslan <e.arslan2025@gmail.com>
2026-06-22 08:57:05 +0200

    Add Google Maps API key

--- data/googleConfig.js
+++ data/googleConfig.js
@@ -1,4 +1,4 @@
 // Optional: Trage hier einen Google Maps JavaScript API-Key ein, der auf
 // https://gena3ekiller.github.io/Urlaubsreise-Juli-2026-Senn/* beschränkt ist.
 // Erforderliche APIs: Maps JavaScript API und Places API.
-window.GOOGLE_MAPS_API_KEY = "";
+window.GOOGLE_MAPS_API_KEY = "AIzaSyC2wXpdiOW4MV3CMq8ceDQilGgMPv1FaQo";
